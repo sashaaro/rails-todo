aad40d7e3a083c2558e97b3ff781712ea68f2862
Aleksandr Arofikin <sashaaro@gmail.com>
2017-01-28 14:36:03 +0000

    fix scss

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,48 @@
 //= require jquery_ujs
 //= require_tree .
 
+// Plugin modal window
+var Modal = function($element) {
+	this.$element = $element;
+	this.showListener = null;
+	this.hideListener = null;
+}
+
+Modal.currencyOpenModal = null
+
+Modal.init = function() { // init modal plugin 
+	$('body').on('click.modal', '.modal-backgroup', this.close.bind(this));
+}
+
+Modal.uninit = function() { // destroy modal plugin 
+	$('body').off('click.modal');
+}
+
+Modal.close = function() { // close current opened window
+	this.currencyOpenModal.hide();
+	this.currencyOpenModal = null;
+}
+
+Modal.prototype.show = function() {
+	$('body').append('<div class="modal-backgroup"></div>');
+	this.$element.get(0).style.display = 'block';
+	Modal.currencyOpenModal = this;
+	if (this.showListener) {
+		this.showListener.call(this);
+	}
+}
+
+Modal.prototype.hide = function() {
+	$('body').children('.modal-backgroup').remove();
+	this.$element.get(0).style.display = 'none';
+	if (this.hideListener) {
+		this.hideListener.call(this);
+	}
+}
+
 $(document).ready(function() {
+	Modal.init();
+
 	var $todoList = $('.well-project-todo');
 
 	$todoList.find('input[type=checkbox]')
@@ -40,29 +81,34 @@ $(document).ready(function() {
   		//allowClear: true
 	};
 
-	var $modal = $('.modal')
-	var $form = $modal.find('form');
-
-	select2Options.dropdownParent = $modal;
-	$form.find('select').select2(select2Options);
-
+	var $addTodoModal = $('.modal.modal-add-todo')
+	var $form = $addTodoModal.find('form');
 	var $showFormButton = $('.btn-show-form');
 	var isSubmited = $form.data('submited');
 
-	$modal.find('select').select2(select2Options);
+	//select2Options.dropdownParent = $addTodoModal;
+	
+	$addTodoModal.find('select').select2(select2Options);
 
-	/*$modal.on('shown.bs.modal', function() {
-		
+	var modalWindow = new Modal($addTodoModal);
+	$showFormButton.on('click', function(e) {
+		e.preventDefault();
+		modalWindow.show();
 	});
 
-	$modal.on('hidden.bs.modal', function() {
+	modalWindow.showListener = function() {
+		$form.find('select').select2(select2Options); // update select2 widget should rerender correct width
+	};
+
+	modalWindow.hideListener = function() { // when close todo form modal window..reset form
 		// Reset form
-		$form.find('.help-block').remove();
+		$form.find('.field-error').remove(); // remove error messages
+		// TODO maybe $form.find('select').select2('reset')
 		$form.trigger('reset');
-	});	*/
+	};
 
 
-	if (isSubmited) {
-		$modal.modal('show');
+	if (isSubmited) { // if submit form was happened. should pre show modal
+		modalWindow.show();
 	}
 });
