12ce76a56440507887772f93bfdcbee7499ea923
Aleksandr Arofikin <sashaaro@gmail.com>
2017-01-29 19:29:29 +0000

    1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,9 @@
 Rails.application.configure do
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = nil
+
+
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
