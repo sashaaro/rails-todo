2df13ac517d628519793208b02b45e38bde9ba0d
Alexandr <sashaaro@gmail.com>
2017-02-07 11:31:39 +0300

    Update 20170125175318_create_todo_entries.rb

--- db/migrate/20170125175318_create_todo_entries.rb
+++ db/migrate/20170125175318_create_todo_entries.rb
@@ -2,7 +2,7 @@ class CreateTodoEntries < ActiveRecord::Migration[5.0]
   def change
     create_table :todo_entries do |t|
       t.string :text
-      t.boolean :isCompleted
+      t.boolean :isCompleted, :default => false
       t.references :project, foreign_key: true
 
       t.timestamps
